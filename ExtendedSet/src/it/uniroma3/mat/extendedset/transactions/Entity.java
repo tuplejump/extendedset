fa452c12b8cfc6d1bd233d18e8898af03539305e
Alessandro Colantonio <colanton@mat.uniroma3.it>
2010-02-26 13:26:09 +0000

    git-svn-id: https://concise.svn.sourceforge.net/svnroot/concise@57 e6cc7dbf-1560-49a9-9a53-a3d9a6477d2e

--- ExtendedSet/src/it/uniroma3/mat/extendedset/transactions/Entity.java
+++ ExtendedSet/src/it/uniroma3/mat/extendedset/transactions/Entity.java
@@ -20,6 +20,37 @@ public interface Entity<A> {
 	 * if the provided index is valid but the entity has no value for the
 	 * corresponding attribute, or raises {@link IndexOutOfBoundsException} if
 	 * the provided index is not valid.
+	 * <p>
+	 * When attributes are represented by several data members of the current
+	 * class, s possible way to implement the method is:
+	 * <p>
+	 * <pre>
+	 *  public List&lt;Set&lt;A&gt;&gt; attributes() {
+	 *      return attributes;
+	 *  }
+	 * 
+	 *  private final List&lt;Set&lt;A&gt;&gt; attributes = new AbstractList&lt;Set&lt;A&gt;&gt;() {
+	 *      public Set&lt;String&gt; get(int index) {
+	 *          switch (index) {
+	 *          case 0:
+	 *              return attr1 == null ? Collections.&lt;A&gt;emptySet() : Collections.singleton(attr1);
+	 *          case 1:
+	 *              return attr2 == null ? Collections.&lt;A&gt;emptySet() : Collections.singleton(attr2);
+	 *          ...
+	 *          ...
+	 *          ...	
+	 *          case 9:
+	 *              return attr9 == null ? Collections.&lt;A&gt;emptySet() : Collections.singleton(attr9);
+	 *          default:
+	 *              throw new IndexOutOfBoundsException(String.valueOf(index));
+	 *          }
+	 *      }
+	 *      
+	 *      public int size() {
+	 *          return 10;
+	 *      }
+	 *  };
+	 * </pre>
 	 * 
 	 * @return the list of properties, or <code>null</code> when the object does
 	 *         not have any property
